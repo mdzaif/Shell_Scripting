e210beaec57a66a77ba2172b2aaadbd574871a17
mdzaif <mdizaif@gmail.com>
2023-12-07 12:18:27 +0600

    Initial upload

---
+++ sys_info.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## Written by Md. Zaif Imam Mahi
+## Last Modified: December 6, 2023
+## Required tools: whoami, hostname, arch, grep, id, awk, groups, bc, echo, uptime, cut, dpkg-query, wc, uname, lspci, paste
+## Environment variables: UID
+## System Files: /etc/os-release, /proc/cpuinfo, /proc/meminfo
+## Worked in: Debian, Ubuntu
+
+
+## variables
+Scale=2
+mt=$(awk -F" " 'NR==1 {print $2}' /proc/meminfo)
+mt=$(echo "scale=$Scale; $mt/1024/1024" | bc -l)
+pac=$(expr $(dpkg-query -l | wc -l) - 5)
+shell=$(echo $SHELL)
+os_name=$(grep -w "ID" /etc/os-release | awk -F'=' '{print $2}')
+
+## colors
+red="\e[31m"
+n="\e[0m"
+
+echo
+
+jp2a ~/my_shell/image/$os_name.jpg --size=50x25 --colors > /tmp/image_ascii.txt
+
+paste /tmp/image_ascii.txt <(printf "\n${red}$(whoami)${n}@${red}$(hostname) $n\
+\n--------------------\n\
+${red}User${n}: $(whoami) \n\
+${red}Group${n}(${red}s${n}): $(groups)\n\
+${red}UID${n}: $UID \n\
+${red}GID${n}: $(id <<< whoami | awk -F'groups=' '{print $2}')\n\
+${red}Uptime${n}: $(uptime -p | cut -c4-)\n\
+${red}OS${n}:  $(awk -F'\"' 'NR==1 {print $2}' /etc/os-release) $(arch)\n\
+${red}Shell${n}: $($shell --version | awk 'NR==1 {print}')\n\
+${red}Kernel${n}: $(uname -r)\n\
+${red}Packages${n}(${red}dpkg${n}): $pac \n\
+${red}CPU${n}: $(grep 'model name' /proc/cpuinfo | awk -F': ' 'NR==2 {print $NR}')\n\
+${red}Memory${n}: total: $mt GB\n\
+${red}GPU${n}: $(lspci -v | grep 'VGA' | awk -F': ' '{print $2}') \n")
+
+rm /tmp/image_ascii.txt
+
+echo
+echo
+
