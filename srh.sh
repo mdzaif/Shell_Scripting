e995f606119a232e3e9343708b3bb3db387edabf
mdzaif <mdizaif@gmail.com>
2023-12-15 23:26:29 +0600

    Last Update: Dec 15, 2023

--- srh.sh
+++ srh.sh
@@ -7,25 +7,27 @@
 ## Colors
 yl='\e[33m'
 bl='\e[34m'
-n="\e[0m"
+n='\e[0m'
 
 usr=$(whoami)
 count=0
 
-printf "Search Result"
-printf "\n-------------------------\n"
-for i in $(find /home/$usr/* -name $1)
+printf "Search Result\n"
+echo -------------------------
+read -p "Enter the file name: " nf
+
+while IFS= read -r -d '' i
 do
-    if [ -f $i ]
-        then
-            printf "${yl}$i : it's a regular file ${n}\n"
-    elif [ -d $i ]
-        then
-            printf "${bl}$i : it's a directory ${n}\n"
+    if [ -f "$i" ]; then
+        printf "${yl}$i : it's a regular file ${n}\n"
+        count=$((count + 1))
     fi
 
-    count=$((count + 1))
-done
+    if [ -d "$i" ]; then
+        printf "${bl}$i : it's a directory ${n}\n"
+        count=$((count + 1))
+    fi
+done < <(find /home/$usr/ -name "$nf" -print0)
 
-printf "\n-------------------------\n"
+echo -------------------------
 printf "Total search result: $count\n"
