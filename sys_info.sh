02b7e8ed23d47934e491dce9eab8fc31bc7463b8
mdzaif <mdizaif@gmail.com>
2025-03-19 22:04:28 +0600

    [FIX]: fix image and gpu finding issue

--- sys_info.sh
+++ sys_info.sh
@@ -4,7 +4,7 @@
 #######################################################################################################################################
 ##                                                                                                                                   ##
 ## Written by Md. Zaif Imam Mahi                                                                                                     ##
-## Last Modified: December 15, 2023                                                                                                   ##
+## Last Modified: March 19, 2025                                                                                                     ##
 ## Add this line to ~/.bashrc: alias info="curl -s https://raw.githubusercontent.com/mdzaif/Shell_For_Fun/main/sys_info.sh | bash"   ##
 ## Idea from neofetch command tool                                                                                                   ##
 ##                                                                                                                                   ##
@@ -24,16 +24,25 @@ mt=$(awk -F" " 'NR==1 {print $2}' /proc/meminfo)
 mt=$(echo "scale=$Scale; $mt/1024/1024" | bc -l)
 shell=$(echo $SHELL)
 os_name=$(grep -w "ID" /etc/os-release | awk -F'=' '{print $2}' | tr -d \" )
+img_link="https://raw.githubusercontent.com/mdzaif/Shell_For_Fun/main/image/$os_name.txt"
+
+if command -v lspci &>/dev/null; then
+        gpu=$(lspci -v | grep 'VGA' | awk -F': ' '{print $2}')
+else
+        gpu="gpu not found"
+fi
 
 ## colors
 red="\e[31m"
 n="\e[0m"
 
-curl https://raw.githubusercontent.com/mdzaif/Shell_For_Fun/main/image/$os_name.txt > /tmp/image_ascii.txt 2> /dev/null
 
-if [ $? -eq 1 ]; then
-	curl https://raw.githubusercontent.com/mdzaif/Shell_For_Fun/main/image/tux_ascii.txt > /tmp/image_ascii.txt
+if curl --output /dev/null --silent --head --fail "$img_link"; then
+        curl https://raw.githubusercontent.com/mdzaif/Shell_For_Fun/main/image/$os_name.txt > /tmp/image_ascii.txt 2> /dev/null
+else
+	curl https://raw.githubusercontent.com/mdzaif/Shell_For_Fun/main/image/tux_ascii.txt > /tmp/image_ascii.txt 2> /dev/null
 fi
+
 
 echo
 
@@ -41,7 +50,7 @@ echo
 paste /tmp/image_ascii.txt <(printf "\n${red}$(whoami)${n}@${red}$(hostname) $n\
 \n--------------------\n\
 ${red}User${n}: $(whoami) \n\
-${red}Group${n}(${red}s${n}): $(groups)\n\
+${red}Group${n} (${red}s${n}): $(groups)\n\
 ${red}UID${n}: $UID \n\
 ${red}GID${n}: $(id <<< whoami | awk -F'groups=' '{print $2}')\n\
 ${red}Uptime${n}: $(uptime -p | cut -c4-)\n\
@@ -49,12 +58,11 @@ ${red}OS${n}:  $(awk -F'\"' 'NR==1 {print $2}' /etc/os-release) $(arch)\n\
 ${red}Shell${n}: $($shell --version | awk 'NR==1 {print}')\n\
 ${red}Kernel${n}: $(uname -r)\n\
 ${red}Packages${n}: $pkg \n\
-${red}CPU${n}: $(grep 'model name' /proc/cpuinfo | awk -F': ' 'NR==2 {print $NR}')\n\
+${red}CPU${n}: $(grep 'model name' /proc/cpuinfo | awk -F': ' 'NR==2 {print $2}')\n\
 ${red}Memory${n}: total: $mt GB\n\
-${red}GPU${n}: $(lspci -v | grep 'VGA' | awk -F': ' '{print $2}') \n")
+${red}GPU${n}: $gpu\n")
 
 rm /tmp/image_ascii.txt
 
 echo
 echo
-
